ee5611c2ff8da68a862300ccf224ecbf78dae13b
sean <ngs1214@gmail.com>
2024-10-31 09:41:00 +0900

    db연결및 회원가입로직 추가

---
+++ src/main/java/com/example/SpringJWT/controller/JoinController.java
@@ -0,0 +1,22 @@
+package com.example.SpringJWT.controller;
+
+import com.example.SpringJWT.dto.JoinDTO;
+import com.example.SpringJWT.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
---
+++ src/main/java/com/example/SpringJWT/dto/JoinDTO.java
@@ -0,0 +1,13 @@
+package com.example.SpringJWT.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class JoinDTO {
+    private String username;
+    private String password;
+}
