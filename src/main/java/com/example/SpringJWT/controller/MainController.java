ceb376aed0788e1a88f41e8c63857ecae0bf0bd7
sean <ngs1214@gmail.com>
2024-10-31 13:24:35 +0900

    jwt 세션정보 확인

--- src/main/java/com/example/SpringJWT/controller/MainController.java
+++ src/main/java/com/example/SpringJWT/controller/MainController.java
@@ -1,14 +1,27 @@
 package com.example.SpringJWT.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
 public class MainController {
     @GetMapping("/")
     public String mainP() {
-        return "Main Controller";
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+
+        return "Main Controller" + name + role;
     }
 }
