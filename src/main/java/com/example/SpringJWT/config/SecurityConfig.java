071f4765e1539d18947841c31795ab0250e61840
sean <ngs1214@gmail.com>
2024-11-04 14:33:11 +0900

    refresh토큰으로 access토큰 재발급

--- src/main/java/com/example/SpringJWT/config/SecurityConfig.java
+++ src/main/java/com/example/SpringJWT/config/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/reissue").permitAll()
                         .anyRequest().authenticated()
                 );
 
